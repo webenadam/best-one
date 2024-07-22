9cf3ef4bea2de0740a00e8b00290ce2d5d95da0f
webenadam <ben@benadam.co.il>
2024-07-22 13:07:29 +0300

    minor fix

--- taxonomy.php
+++ taxonomy.php
@@ -36,30 +36,7 @@ background-position: top right; background-repeat: no-repeat; background-color: 
 get_template_part('templates/main-feed', null, array('featured_pros' => $featured_pros));
 ?>
 
-<section id="features" style="border-bottom:1px solid var(--light-gray);">
-  <inner class="flex gap-l align-center" style="padding-bottom:0;">
-    <right class="flex"><img src="<?= theme_uri('/img/whyus_man.jpg'); ?>" alt="למה אצלנו?"></right>
-    <left style="margin-top:-10px;">
-      <h2>למה אצלנו?</h2>
-      <p style="color:var(--dark-gray); width:480px;margin-bottom: 40px;">באתר BEST1 תמצאו את כל בעלי המקצוע הטובים ביותר מסודרים בצורה נוחה וקלה לחיפוש בכל התחומים.</p>
-      <grid class="grid-2 gap-s">
-        <style>
-          #features h3.check::before {
-            content: url('data:image/svg+xml;charset=utf-8,%3Csvg%20width%3D%2218%22%20height%3D%2213%22%20viewBox%3D%220%200%2018%2013%22%20fill%3D%22none%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%0A%20%20%3Cpath%20d%3D%22M6.57216%2012.4615C6.2614%2012.4615%205.95124%2012.3449%205.71392%2012.1109L0%206.47876L1.71648%204.78623L6.57216%209.57245L16.2835%200L18%201.69253L7.4304%2012.1109C7.19308%2012.3449%206.88292%2012.4615%206.57216%2012.4615Z%22%20fill%3D%22%23473BF0%22%20%2F%3E%0A%3C%2Fsvg%3E');
-            margin-left: 13px;
+<?php get_template_part('templates/features'); ?>
 
-          }
-        </style>
-        </style>
-        <h3 class="check">רק בעלי מקצוע מוסכמים</h3>
-        <h3 class="check">רק עם תעודה בתוקף</h3>
-        <h3 class="check">סינון קל ונוח לפי תחומים</h3>
-        <h3 class="check">פרטים מלאים כל כל בעל מקצוע</h3>
-        <h3 class="check">יצירת קשר ישירה</h3>
-        <h3 class="check">פרטים מעודכנים תמיד</h3>
-      </grid>
-    </left>
-  </inner>
-</section>
 
 <?php get_footer(); ?>
