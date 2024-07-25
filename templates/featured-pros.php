ab6f597c006e5632a00fa62bd0a74f7306f69a94
webenadam <ben@benadam.co.il>
2024-07-25 15:12:15 +0300

    Fancy featured pros

--- templates/featured-pros.php
+++ templates/featured-pros.php
@@ -1,11 +1,130 @@
+<style>
+  .leaves_left,
+  .leaves_right {
+    position: absolute;
+    top: 80px;
+  }
+
+  .leaves_left svg,
+  .leaves_right svg {
+    width: 250px;
+    fill: var(--blue);
+  }
+
+  .leaves_left svg path,
+  .leaves_right svg path {
+    fill: var(--light-green);
+  }
+
+  .leaves_right {
+    right: 5%;
+    animation: slight-rotate 7s infinite ease-in-out;
+  }
+
+  .leaves_left {
+    left: 5%;
+    animation: slight-rotate-reverse 7s infinite ease-in-out;
+  }
+
+
+  @media (min-width: 850px) {
+  #featured-pros .profile-box {
+    animation: wave 2.5s infinite ease-in-out;
+    z-index:90;
+  }
+
+  #featured-pros .profile-box:nth-child(2) {
+    animation-delay: 0s;
+  }
+
+  #featured-pros .profile-box:nth-child(3) {
+    animation-delay: 0.2s;
+  }
+
+  #featured-pros .profile-box:nth-child(4) {
+    animation-delay: 0.4s;
+  }
+  #featured-pros .profile-box:hover {
+    border-color: var(--green);
+  }
+}
+
+@keyframes wave {
+  0%, 33.33%, 100% {
+    transform: translateY(0);
+  }
+  16.67% {
+    transform: translateY(-10px);
+  }
+}
+
+@keyframes slight-rotate {
+  0% {
+    transform: rotateZ(5deg) rotateY(5deg);
+  }
+  50% {
+    transform: rotateZ(-5deg) rotateY(-28deg);
+  }
+  100% {
+    transform: rotateZ(5deg) rotateY(5deg);
+  }
+}
+
+@keyframes slight-rotate-reverse {
+  0% {
+    transform: rotateZ(-5deg) rotateY(-5deg);
+  }
+  50% {
+    transform: rotateZ(5deg) rotateY(28deg);
+  }
+  100% {
+    transform: rotateZ(-5deg) rotateY(-5deg);
+  }
+}
+
+@keyframes shine {
+  0% {
+    background-position: -200% 0;
+  }
+  100% {
+    background-position: 200% 0;
+  }
+}
+
+#featured-pros {
+    background: linear-gradient(100deg, var(--soft-background), #68d5854a, var(--soft-background));
+    background-size: 200% 100%;
+    animation: shine 6s linear infinite;
+    overflow: hidden;
+}
+
+#featured-pros::after {
+  content: '';
+  background: var(--soft-background);
+  width: 140%;
+  position: absolute;
+  bottom:-60px;
+  left:-20%;
+  right: -20% ;
+  height: 250px ;
+  filter: blur(85px);
+  pointer-events: none;
+  opacity:1;
+}
+
+
+
+</style>
 <?php
 $section_classes = isset($args['section_classes']) ? $args['section_classes'] : '';
 ?>
-<section id="featured-pros" class="align-center <?php echo $section_classes;?>">
+<section id="featured-pros" class="align-center relative <?php echo $section_classes; ?>">
   <inner>
-    <h2 class="black"><span class="sparkle" style=" margin-right: -25px; margin-bottom: 25px; display: inline-block; transform: translateY(-14px); "><?= svg_icon('sparkle'); ?></span>המומלצים שלנו</h2>
+    <span class="leaves_right"><?= svg_icon('leaves',null,'flip-h' ); ?></span>
+    <span class="leaves_left"><?= svg_icon('leaves'); ?></span>
+    <h2 class="black font-800 text-center">המומלצים שלנו ל-<?php echo date('Y'); ?></h2>
     <grid class="grid-3 relative">
-    <div class="circle absolute" style="top:180px; right:60%; width:235px;height:235px;border-radius:50%;background:var(--light-green);"></div>
+      <div class="circle absolute grow-shrink" style="z-index:40;top:180px; right:60%; width:235px;height:235px;border-radius:50%;background:var(--light-green);"></div>
       <?php
       // Get featured pros from site settings
       $featured_pros = get_field('home_featured_pros', 'option');
--- templates/features.php
+++ templates/features.php
@@ -1,6 +1,6 @@
 <section id="features" style="overflow:hidden;margin-top: -290px;">
   <inner class="flex mobile-flex-column-reverse gap-l align-center" style="padding-bottom:0;">
-    <right class="flex"><div class="men-container relative" style="margin-bottom:-50px;"><div class="circle absolute" style="top:180px; right:-70px; width:235px;height:235px;border-radius:50%;background:var(--green);"></div><img style="position:relative; z-index:5; width:400px;" src="<?= theme_uri('/img/2men3.png'); ?>" alt="למה אצלנו?"></div></right>
+    <right class="flex"><div class="men-container relative" style="margin-bottom:-50px;"><div class="circle absolute grow-shrink" style="top:220px; right:-70px; width:235px;height:235px;border-radius:50%;background:var(--green);"></div><img style="position:relative; z-index:5; width:400px;" src="<?= theme_uri('/img/2men3.png'); ?>" alt="למה אצלנו?"></div></right>
     <left style="margin-top:-10px;">
       <h2>למה אצלנו?</h2>
       <style>
